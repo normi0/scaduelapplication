4e60818aaa23a232851695b0ea0908b7e83f4fd7
achi seif <root@DESKTOP-37GK2LD.>
2024-11-29 18:00:54 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// const user = fetch("https://jsonplaceholder.typicode.come/users");
+
+// console.log(user);
+
+// fetch("https://jsonplaceholder.typicode.come/users")
+// .then(Response =>{
+//     return Response.json();
+// })
+// .then(data =>{
+//     console.log(user);
+    
+// })
